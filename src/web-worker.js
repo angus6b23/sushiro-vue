2a7f65ce01685e083c097fbb8eb974a545a1c626
Angus Wan <work@12a.app>
2022-09-30 20:14:29 +0800

    Attempt to use web worker as service worker

--- src/web-worker.js
+++ src/web-worker.js
@@ -1,13 +1,17 @@
 
-var notice = ''
-var repeatNotice = ''
+var notice = '';
+var repeatNotice = '';
+self.addEventListener('install', function(event) {
+    // Perform some task
+});
+
 onmessage = async (e) =>{
     let message = e.data;
     if (message.cancel){
         clearInterval(reapeatNotice);
     } else {
         let nextTicket = generateInfo(message);
-        reapeatNotice = setInterval(function(){generateInfo(message)}, 5000);
+        reapeatNotice = setInterval(function(){generateInfo(message)}, 30000);
     }
 }
 
@@ -16,7 +20,10 @@ async function generateInfo(message){
         let response = await fetch(message.url);
         let fullfilled = await response.json();
         let nextTicket = await fullfilled.storeQueue[0];
-        notice = new Notification('壽司郎\n' + message.storeName + '：' + nextTicket + '\n按下通知取消追蹤');
+        notice = new Notification('壽司郎', {
+            tag: 'sushiro',
+            body: message.storeName + '：' + nextTicket
+        });
     } catch(e){
         console.error(e);
     }
