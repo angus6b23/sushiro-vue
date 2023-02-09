e23b6a64579ac69eb5df34913c4cf2519566fab0
Angus Wan <work@12a.app>
2023-02-09 23:24:09 +0800

    Fix router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,13 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
 import { RouteRecordRaw } from 'vue-router';
+import HomePage from '../views/HomePage.vue'
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
-    redirect: '/'
-  },
+    name: "Home",
+    component: HomePage
+  }
 ]
 
 const router = createRouter({
